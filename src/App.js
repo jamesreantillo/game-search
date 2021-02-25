6c9b04b2bca46d8c476b83b6be89e10665b93d60
james reantillo <james.reantillo@gmail.com>
2021-02-25 01:46:19 -0800

    add global style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 //Components and pages
 import Home from './pages/Home';
+import Globalstyles from './components/GlobalStyles';
 
 function App() {
   return (
     <div className='App'>
+      <Globalstyles />
       <Home />
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,9 +19,33 @@ const Home = () => {
 
   return (
     <GameList>
-      <h2>Home</h2>
+      <h2>Upcoming Games</h2>
       <Games>
         {upcoming.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+      <h2>Popular</h2>
+      <Games>
+        {popular.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+      <h2>New Games</h2>
+      <Games>
+        {newGames.map((game) => (
           <Game
             name={game.name}
             released={game.released}

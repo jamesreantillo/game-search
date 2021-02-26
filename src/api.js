53b682808bd47d03c6508c1e808dbdc036c96905
james reantillo <james.reantillo@gmail.com>
2021-02-26 00:12:32 -0800

    fetch game detail

--- src/api.js
+++ src/api.js
@@ -37,3 +37,9 @@ const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page
 export const popularGamesUrl = () => `${base_url}${popular_games}`;
 export const upcomingGamesUrl = () => `${base_url}${upcoming_games}`;
 export const newGamesUrl = () => `${base_url}${newGames}`;
+
+//Game Detail
+export const gameDetailsUrl = (game_id) => `${base_url}games/${game_id}`;
+//Game Screenshot
+export const gameScreenshotUrl = (game_id) =>
+  `${base_url}games/${game_id}/screenshots`;

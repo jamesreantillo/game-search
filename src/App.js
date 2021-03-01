50662ae4e1f456e2dbd9ec0c663e87d196043fd7
james reantillo <james.reantillo@gmail.com>
2021-03-01 12:19:58 -0800

    implement preloding

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 //Components and pages
 import Home from './pages/Home';
 import Globalstyles from './components/GlobalStyles';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
       <Globalstyles />
-      <Home />
+      <Route path={['/game/:id', '/']}>
+        <Home />
+      </Route>
     </div>
   );
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -5,6 +5,7 @@ import { motion } from 'framer-motion';
 //Redux
 import { useDispatch } from 'react-redux';
 import { loadDetail } from '../actions/detailAction';
+import { Link } from 'react-router-dom';
 
 const Game = ({ name, released, image, id }) => {
   //Load Detail
@@ -16,9 +17,11 @@ const Game = ({ name, released, image, id }) => {
 
   return (
     <StyledGame onClick={loadDetailHandler}>
-      <h3>{name}</h3>
-      <p>{released}</p>
-      <img src={image} alt={name} />
+      <Link to={`/game/${id}`}>
+        <h3>{name}</h3>
+        <p>{released}</p>
+        <img src={image} alt={name} />
+      </Link>
     </StyledGame>
   );
 };
@@ -28,25 +31,12 @@ const StyledGame = styled(motion.div)`
   box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
   text-align: center;
   border-radius: 1rem;
+  cursor: pointer;
   img {
     width: 100%;
     height: 40vh;
     object-fit: cover;
   }
 `;
-
-// const StyledGame = styled(motion.div)`
-//   min-height: 30vh;
-//   box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
-//   text-align: center;
-//   border-radius: 1rem;
-//   cursor: pointer;
-//   overflow: hidden;
-//   img {
-//     width: 100%;
-//     height: 40vh;
-//     object-fit: cover;
-//   }
-// `;
 
 export default Game;

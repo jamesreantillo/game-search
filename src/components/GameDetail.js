7f770ad995e8bfde10e1824beb74ddaa0b307f66
james reantillo <james.reantillo@gmail.com>
2021-03-05 00:50:30 -0800

    add icons

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -6,6 +6,13 @@ import { motion } from 'framer-motion';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { smallImage } from '../util';
+//Images
+import playstation from '../images/playstation.svg';
+import steam from '../images/steam.svg';
+import xbox from '../images/xbox.svg';
+import nintendo from '../images/nintendo.svg';
+import apple from '../images/apple.svg';
+import gamepad from '../images/gamepad.svg';
 
 const GameDetail = ({ pathId }) => {
   const history = useHistory();
@@ -15,6 +22,24 @@ const GameDetail = ({ pathId }) => {
     if (element.classList.contains('shadow')) {
       document.body.style.overflow = 'auto';
       history.push('/');
+    }
+  };
+  //Get platform images
+
+  const getPlatform = (platform) => {
+    switch (platform) {
+      case 'Playstation 4':
+        return playstation;
+      case 'Xbox One':
+        return xbox;
+      case 'PC':
+        return steam;
+      case 'Nintendo Switch':
+        return nintendo;
+      case 'iOS':
+        return apple;
+      default:
+        return gamepad;
     }
   };
   //Data
@@ -33,7 +58,11 @@ const GameDetail = ({ pathId }) => {
                 <h3>Platforms</h3>
                 <Platforms>
                   {game.platforms.map((data) => (
-                    <h3 key={data.platform.id}>{data.platform.name}</h3>
+                    <img
+                      key={data.platform.id}
+                      src={getPlatform(data.platform.name)}
+                      alt={data.platform.name}
+                    />
                   ))}
                 </Platforms>
               </Info>
